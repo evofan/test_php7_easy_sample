d1cd59149b4462087f599bfd7c1eff18ce93de32
evofan <evofan@outlook.jp>
2021-09-01 23:38:53 +0900

    Fix

---
+++ chap3/template/sample_01b.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<title>よくわかるPHPの教科書</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">よくわかるPHPの教科書</h1>    
+</header>
+
+<main>
+<h2>Practice</h2>
+	
+<h3>エスケープシーケンス</h3>
+	
+<pre>
+<?php
+/* ここに、PHPのプログラムを記述します */
+/* エスケープシーケンスを囲う時は”で囲む */
+print('PHP勉強中です');
+print("\n\n");
+print('I\'m studying PHP');
+?>
+</pre>
+</main>
+</body>    
+</html>
